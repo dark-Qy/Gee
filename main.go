7ab1a1d64837cf52aee5a6886b5929a69dfa1c32
dark-Qy <2454271718@qq.com>
2025-02-10 16:19:51 +0800

    实现ServeHTTP接口

--- main.go
+++ main.go
@@ -5,23 +5,30 @@ import (
 	"net/http"
 )
 
-func main() {
-	// 创建两个路由，第一个路由直接打印当前的路径，第二个打印当前路由的请求头所有内容
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	// 启动监听程序
-	http.ListenAndServe(":8080", nil)
+/*
+type Handler interface {
+	ServeHTTP(ResponseWriter, *Request)
 }
+*/
 
-// handler echoes r.URL.Path
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Method = %q\n", req.Method)
-}
+// Engine 自定义类实现ServerHTTP接口
+type Engine struct{}
 
-// handler echoes r.URL.Header
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	// 循环打印req.Header中的所有内容
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Header[%q]=%q\n", k, v)
+// Engine实现ServerHTTP接口
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	case "/hello":
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
 	}
+}
+
+func main() {
+	engine := &Engine{}
+	http.ListenAndServe(":8080", engine)
 }
