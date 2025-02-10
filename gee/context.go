62f5a74e56d854fe8326dcc17e5868f73725edba
dark-Qy <2454271718@qq.com>
2025-02-10 21:50:58 +0800

    新增上下文内容

---
+++ gee/context.go
@@ -0,0 +1,82 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// H 设置H作为一个类型别名，用于简化操作
+type H map[string]interface{}
+
+// Context 上下文
+type Context struct {
+	// 原始请求和响应对象
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// 请求信息
+	Path   string
+	Method string
+	// 状态码
+	StatusCode int
+}
+
+// NewContext 创建一个新的上下文
+func NewContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    r,
+		Path:   r.URL.Path,
+		Method: r.Method,
+	}
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+func (c *Context) SetHeader(key string, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+// 提供访问Query和PostForm参数的方法
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+// 提供了快速构造String/Data/JSON/HTML响应的方法
+// String 返回字符串，根据指定格式排列
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+// JSON 返回JSON格式数据
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	// 序列化
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+// HTML 返回HTML格式数据
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
